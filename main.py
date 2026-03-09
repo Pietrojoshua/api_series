6f9ce41a76ee537f11e49847938c20cc38a99415
Pietrojoshua <pietrocarlosmunoz12@gmail.com>
2026-03-09 16:53:36 -0300

    mexendo no FastAPI

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from api-series!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/itens/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+
+    return {"item_id": item_id, "q": q}
+
+@app.get("/soma/{a}/{b}")
+def soma(a:int, b: int):
+    return {"resultado": a + b}
